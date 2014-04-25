52fa8b987d79497947f9f3637763015a632d12ff
jeturcotte <joshua.eric.turcotte@gmail.com>
2014-04-25 12:24:22 -0400

    a little better

--- Ruin.pm
+++ Ruin.pm
@@ -28,6 +28,30 @@ package Ruin {
 		trigger => \&_read_and_test_files
 	);
 	
+	has 'rule_count' => (
+		is => 'rw',
+		isa => 'HashRef',
+		default => sub {{
+			'0.0.0' => 0,
+			'255.255.255' => 0
+		}}
+	);
+	
+	has 'rule_id' => (
+		is => 'rw',
+		isa => 'HashRef',
+		default => sub {{
+			'0.0.0' => 0,
+			'255.255.255' => 1
+		}}
+	);
+	
+	has 'matrix' => (
+		is => 'rw',
+		isa => 'ArrayRef',
+		default => sub {[]}
+	);
+	
 	sub _read_and_test_files {
 		my ($self, $loc, $old) = @_;
 		opendir (DIR, $loc) or die $!;
@@ -38,10 +62,12 @@ package Ruin {
 		}
 		$self->depth(scalar @files);
 		
+		my $pz = 0;
 		foreach my $file(@files){
 			my $floc = $self->image_location."/$file";
 			my $i = Imager->new(file => $floc)
 				or panic("$floc is not a valid image!");
+				
 			my $x = $i->getwidth;
 			my $y = $i->getheight;
 			if ($self->width and $self->height) {
@@ -52,9 +78,25 @@ package Ruin {
 				$self->height($y);
 			}
 			
+			my @slab;
+			foreach my $py(0..$self->height-1){
+				my @row;
+				foreach my $px(0..$self->width-1){
+					my ($r, $g, $b, undef) = $i->getpixel(x => $px, y => $py)->rgba();
+					my $c = "$r.$g.$b";
+					$self->rule_count->{$c} += 1;
+					$self->rule_id->{$c} = scalar(keys %{$self->rule_id})+1
+						unless defined($self->rule_id->{$c});
+					push @row, $self->rule_id->{$c};
+				}
+				push @slab, join(",",@row);
+			}
+			
+			push @{$self->matrix}, "\nlayer\n".join("\n",@slab)."\nendlayer\n";
+			
 		}
 		
-		grin("Ruin successfully initialized at ".$self->width()." voxels wide by ".$self->height()." voxels long by ".$self->depth()." voxels tall");	
+		grin("Ruin successfully initialized with ".scalar(keys %{$self->rule_id})." rules at ".$self->width()." voxels wide by ".$self->height()." voxels long by ".$self->depth()." voxels tall");	
 	}
 }
 
--- image2tml.pl
+++ image2tml.pl
@@ -7,3 +7,5 @@ use Colorful;
 use feature 'say';
 
 my $ruin = Ruin->new(image_location => $ARGV[0] || './png_layers');
+
+say join("\n", @{$ruin->matrix});
